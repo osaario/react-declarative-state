07d5d732f18c9e986e84abb4caed5c6441177d1e
osaario <osaario1@gmail.com>
2018-10-28 10:20:30 +0200

    add 1 test

--- test/declarative-components.test.ts
+++ test/declarative-components.test.ts
@@ -1,14 +1,11 @@
-import DummyClass from "../src/declarative-components"
+import { notEmpty } from '../src/formrules'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+describe('Form rules tests', () => {
+  it('notEmpty tests', () => {
+    expect(notEmpty('', true)).toBeTruthy()
+    expect(notEmpty('geg', true)).toBeNull()
   })
 })
