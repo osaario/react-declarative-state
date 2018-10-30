254d63200a6538865a4dba71fb38d8f58baf174e
osaario <osaario1@gmail.com>
2018-10-30 20:20:31 +0200

    add shadows to purescope

--- src/PureScope.tsx
+++ src/PureScope.tsx
@@ -1,29 +1,21 @@
 import * as React from 'react'
+import _ from 'lodash'
 import { shallowCompareInjections } from './injections'
 
-export type PureScopeProps<E extends object> = {
+export type PureScopeProps<E extends object, F extends object> = {
   injections: E
-  shadows?: object
-  children: (injections: E) => JSX.Element
+  shadows?: F
+  children: (injections: E, shadows: { [K in keyof F]: null }) => JSX.Element
 }
 
-export class PureScope<E extends object> extends React.Component<PureScopeProps<E>> {
-  shouldComponentUpdate(nextProps: PureScopeProps<E>) {
+export class PureScope<E extends object, F extends object> extends React.Component<PureScopeProps<E, F>> {
+  shouldComponentUpdate(nextProps: PureScopeProps<E, F>) {
     if (!shallowCompareInjections(nextProps.injections, this.props.injections)) {
       return false
     }
     return true
   }
   render() {
-    if (this.props.shadows) {
-      const str = this.props.children.toString()
-      const foundShadows = Object.keys(this.props.shadows).reduce((acc, shadowKey) => {
-        return str.indexOf(' ' + shadowKey) !== -1 || acc
-      }, false)
-      if (foundShadows) {
-        throw Error('Shadowed variables found in function')
-      }
-    }
-    return this.props.children(this.props.injections)
+    return this.props.children(this.props.injections, _.mapValues(this.props.shadows, () => null))
   }
 }
