171de8b13df139e1d442e716440f1c4321de3a58
osaario <osaario1@gmail.com>
2018-11-08 15:26:52 +0200

    fix build

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,7 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['lodash', 'rxjs', 'react', 'react-dom', 'partial.lenses', 'is-promise', 'symbol-observable'],
+  external: ['rxjs', 'react', 'react-dom', 'is-promise', 'symbol-observable'],
   watch: {
     include: 'src/**'
   },
